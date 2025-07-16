7e80607dfd625d5abf717f1657381f08341ad6b8
Rich Man <uriemanda7@gmail.com>
2025-07-16 11:59:41 +0700

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,1 +1,16 @@
-export default { darkMode: 'class', content: ['./app/**/*.{js,ts,jsx,tsx}'] }
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    "./app/**/*.{ts,tsx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [],
+};
+
+export default config;
