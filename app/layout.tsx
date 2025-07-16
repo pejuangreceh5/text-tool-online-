d118d1499408b97d0cff1a4f8202566bc8ffb4b3
Rich Man <uriemanda7@gmail.com>
2025-07-16 11:41:32 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,10 @@
-<html>/* Layout utama */</html>
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head />
+      <body className="bg-white text-black dark:bg-gray-900 dark:text-white">
+        {children}
+      </body>
+    </html>
+  );
+}
